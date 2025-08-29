ad2593a61c57dbd1b1495074a9416aa1a181954a
Delgorithm <delgorithm@gmail.com>
2025-08-29 10:52:46 +0200

    feat - page : blind 75 topics

---
+++ lua/leetcode-ui/group/page/backtracking-75.lua
@@ -0,0 +1,40 @@
+local cmd = require("leetcode.command")
+
+local Title = require("leetcode-ui.lines.title")
+local Button = require("leetcode-ui.lines.button.menu")
+local BackButton = require("leetcode-ui.lines.button.menu.back")
+local Buttons = require("leetcode-ui.group.buttons.menu")
+local Page = require("leetcode-ui.group.page")
+
+local footer = require("leetcode-ui.lines.footer")
+local header = require("leetcode-ui.lines.menu-header")
+
+local page = Page()
+
+page:insert(header)
+
+page:insert(Title({ "Blind 75" }, "Backtracking"))
+
+local list = Button("List", {
+  icon = "",
+  sc = "p",
+  on_press = cmd.backtracking_75()
+})
+
+local random = Button("Random", {
+  icon = "",
+  sc = "r",
+  on_press = cmd.random_backtracking_75(),
+})
+
+local back = BackButton("problems")
+
+page:insert(Buttons({
+  list,
+  random,
+  back,
+}))
+
+page:insert(footer)
+
+return page
---
+++ lua/leetcode-ui/group/page/heap-and-priority-queue-75.lua
@@ -0,0 +1,34 @@
+local cmd = require("leetcode.command")
+
+local Title = require("leetcode-ui.lines.title")
+local Button = require("leetcode-ui.lines.button.menu")
+local BackButton = require("leetcode-ui.lines.button.menu.back")
+local Buttons = require("leetcode-ui.group.buttons.menu")
+local Page = require("leetcode-ui.group.page")
+
+local footer = require("leetcode-ui.lines.footer")
+local header = require("leetcode-ui.lines.menu-header")
+
+local page = Page()
+
+page:insert(header)
+
+page:insert(Title({ "Blind 75" }, "Heap & Priority Queue"))
+
+local list = Button("List", {
+  icon = "",
+  sc = "p",
+  on_press = cmd.heap_and_priority_queue_75()
+})
+
+
+local back = BackButton("problems")
+
+page:insert(Buttons({
+  list,
+  back,
+}))
+
+page:insert(footer)
+
+return page
